582ad49010c952590cca5fa0042630feda051942
panzerstadt <tliqun@gmail.com>
2019-06-14 15:47:29 +0900

    using vue-resource

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,8 +17,6 @@
 </template>
 
 <script>
-import axios from "axios";
-
 export default {
   name: "HelloWorld",
   data() {
@@ -33,9 +31,9 @@ export default {
   },
   mounted() {
     // compoinentDidMount
-    axios({ method: "GET", url: "https://httpbin.org/ip" }).then(
+    this.$http.get("https://httpbin.org/ip").then(
       result => {
-        this.ip = result.data.origin.split(",")[0];
+        this.ip = result.body.origin.split(",")[0];
       },
       error => {
         this.response = JSON.parse(error);
@@ -44,23 +42,22 @@ export default {
   },
   methods: {
     sendData() {
-      axios({
-        method: "POST",
-        url: "https://httpbin.org/post",
-        data: this.input,
-        headers: { "content-type": "application/json" }
-      }).then(
-        result => {
-          this.response = JSON.stringify(
-            JSON.parse(result.data.data),
-            false,
-            2
-          );
-        },
-        error => {
-          this.response = JSON.parse(error);
-        }
-      );
+      this.$http
+        .post("https://httpbin.org/post", this.input, {
+          "content-type": "application/json"
+        })
+        .then(
+          result => {
+            this.response = JSON.stringify(
+              JSON.parse(result.data.data),
+              false,
+              2
+            );
+          },
+          error => {
+            this.response = JSON.parse(error);
+          }
+        );
     }
   }
 };
