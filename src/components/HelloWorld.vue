4f7f6d32a521b8cdb1022f1ec8a16a0a2b99b21e
panzerstadt <tliqun@gmail.com>
2019-06-14 15:38:54 +0900

    api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,93 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>Your IP is {{ip}}</h1>
+    <input type="text" v-model="input.firstname" placeholder="First Name">
+    <input type="text" v-model="input.lastname" placeholder="Last Name">
+    <button v-on:click="sendData()">Send</button>
+    <br>
+    <br>
+    <textarea name="response" id="response" cols="30" rows="10" v-model="response"></textarea>
+    <br>
+    <a
+      target="_blank"
+      rel="noopener noreferer"
+      href="https://www.thepolyglotdeveloper.com/2017/10/consume-api-data-http-vuejs-web-application/"
+    >tutorial from here</a>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  data() {
+    return {
+      ip: "",
+      input: {
+        firstname: "",
+        lastname: ""
+      },
+      response: ""
+    };
+  },
+  mounted() {
+    // compoinentDidMount
+    axios({ method: "GET", url: "https://httpbin.org/ip" }).then(
+      result => {
+        this.ip = result.data.origin.split(",")[0];
+      },
+      error => {
+        this.response = JSON.parse(error);
+      }
+    );
+  },
+  methods: {
+    sendData() {
+      axios({
+        method: "POST",
+        url: "https://httpbin.org/post",
+        data: this.input,
+        headers: { "content-type": "application/json" }
+      }).then(
+        result => {
+          this.response = JSON.stringify(
+            JSON.parse(result.data.data),
+            false,
+            2
+          );
+        },
+        error => {
+          this.response = JSON.parse(error);
+        }
+      );
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h1,
+h2 {
+  font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
-  color: #42b983;
+  color: "42b983";
+}
+
+textarea {
+  width: 600px;
+  height: 200px;
 }
 </style>
